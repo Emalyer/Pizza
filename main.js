0c8c1ac5e6b5b246b238763cc9fa5e960ecf294f
Emalyer <77217260+Emalyer@users.noreply.github.com>
2021-10-13 16:26:19 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,32 @@
+menu_list_array=["Chicken Tandoori Pizza",
+                 "Veg Supreme Pizza",
+                 "Paneer Tikka Pizza",
+                 "Deluxe Veggie Pizza",
+                 "Veg Extravaganza Pizza"];
+
 function getmenu(){
-var htmldata;
-htmldata="<ol class='menulist>"
-menu_list_array.sort();
-menu_list_array.push();
-for(var i=0;i<menu_list_array.length;i++){
+    var htmldata;
+    htmldata="<ol class='meanulist'>"
+    menu_list_array.sort();
+    for(var i=0;i<menu_list_array.length;i++){
+       htmldata=htmldata+"'<li>'"+menu_list_array[i]+'</li>'
+       
+    }
     htmldata=htmldata+"</ol>"
-    document.getElementById("display_menu").innerHTML=htmldata;
+    document.getElementById("display_menu").innerHtml=htmldata;
 }
+function add_item(){
+    var htmldata;
+    var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+    menu_list_array.sort();
+    htmldata="<section class='cards'>"
+    for(var i=0;i<menu_list_array.length;i++){
+
+        htmldata-htmldata+'<div class="card">'+'<img src="images/pizzaImg.png"/>'+menu_list_array[i]+'</div>'
+    }
+    htmldata=htmldata+"</section>"
+    document.getElementById("display_addedmenu").innerHTML=htmldata;
 }
 
-function additem(){
-    menu_list_array=["Chicken Tandoori Pizza",
-    "Veg Sumpreme Pizza",
-    "Paneer Tikka Pizza",
-   "Deluxe Veggie Pizza",
-   "Veg Extravaganza Pizza"];
-}
 
